a8341b36625eaea58f371bc13479b95418039039
mark harrison <darthmalick@gmail.com>
2022-06-25 13:44:07 -0500

    commit tree 448dc8539bdb8c09b1b1363d28ff248039ce97f7

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const LeftNumbers = dodger.stytle.left.replace("px", "");
+    const left = parseint(LeftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left -1}px`;
+    }
+};
+document.addEventListener("keydown", function (e) {
+    if (e.key === "arrowleft") {
+        moveDodgerLeft();
+    }
+});
+function moveDodgerRight(){
+    const leftNumbers =dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers, 10);
+if (left > 179){
+dodger.style.left = `${left+1}px`;
+}
+};
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
